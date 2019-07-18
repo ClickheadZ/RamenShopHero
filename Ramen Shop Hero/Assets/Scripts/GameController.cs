1702141016c91ceab2ad6af5797122bcdb3883ca
ClickheadZ <ClickheadZ@users.noreply.github.com>
2019-07-18 19:03:50 -0400

    Canvas swapping tests successful

--- Ramen Shop Hero/Assets/Scripts/GameController.cs
+++ Ramen Shop Hero/Assets/Scripts/GameController.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     //This class should control the game state, display the proper canvas, and reference the Scoring script.
+    //Needs reference to each canvas
 
-    private enum GameState { Orders, Ingredients, Cooking };
+    private Canvas orderCanvas;
+    private Canvas conversationCanvas;
+    private Canvas ingredientCanvas;
+    private Canvas cookingPreludeCanvas;
+    private Canvas cookingCanvas;
 
-    void Awake()
+    void Start()
     {
-        
+        orderCanvas = GameObject.Find("OrderCanvas").GetComponent<Canvas>();
+        conversationCanvas = GameObject.Find("ConversationCanvas").GetComponent<Canvas>();
+
+        DisableConversationCanvas();
     }
 
-    void Update()
+    public void EnableOrderCanvas()
     {
-        
+        orderCanvas.gameObject.SetActive(true);
+    }
+
+    public void DisableOrderCanvas()
+    {
+        orderCanvas.gameObject.SetActive(false);
+    }
+
+    public void EnableConversationCanvas()
+    {
+        conversationCanvas.gameObject.SetActive(true);
+    }
+
+    public void DisableConversationCanvas()
+    {
+        conversationCanvas.gameObject.SetActive(false);
     }
 }
